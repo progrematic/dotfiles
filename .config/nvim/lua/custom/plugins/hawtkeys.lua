1c161e1a4ee35c0d6b9541a06680ec40df873b2c
progrematic <20295773+progrematic@users.noreply.github.com>
2026-02-12 08:17:28 -0800

    fixing hawtkeys warning

--- .config/nvim/lua/custom/plugins/hawtkeys.lua
+++ .config/nvim/lua/custom/plugins/hawtkeys.lua
@@ -4,7 +4,7 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
-  config = {
+  opts = {
     -- an empty table will work for default config
     --- if you use functions, or whichkey, or lazy to map keys
     --- then please see the API below for options
